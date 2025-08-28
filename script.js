3e3964e0456131316363978d8aa5a668946a0d75
Tanvir-programmer <tanvir.webprogrammer@gmail.com>
2025-08-28 10:32:23 +0300

    coin and heart functionality done!

--- script.js
+++ script.js
@@ -10,15 +10,124 @@ cardsContainer.addEventListener("click", (event) => {
     heartCountElement.textContent = heartCount;
   }
 });
+// coin count
 
+const availableCoin = parseInt(document.getElementById("coin-count").innerText);
 // call button functionallity
-
-const callBtn = document
-  .getElementById("callBtn")
-  .addEventListener("click", (event) => {
-    event.preventDefault();
-    const availableCoin = document.getElementById("coin-count").innerText;
-    const availableCoiNumber = parseInt(availableCoin);
-    const finalCount = availableCoin - 20;
-    console.log(finalCount);
-  });
+// card-1
+document.getElementById("callBtn-1").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-1").innerText;
+  const number = document.getElementById("number-1").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-2
+document.getElementById("callBtn-2").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-2").innerText;
+  const number = document.getElementById("number-2").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-3
+document.getElementById("callBtn-3").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-3").innerText;
+  const number = document.getElementById("number-3").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-4
+document.getElementById("callBtn-4").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-4").innerText;
+  const number = document.getElementById("number-4").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-5
+document.getElementById("callBtn-5").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-5").innerText;
+  const number = document.getElementById("number-5").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-6
+document.getElementById("callBtn-6").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-6").innerText;
+  const number = document.getElementById("number-6").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-7
+document.getElementById("callBtn-7").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-7").innerText;
+  const number = document.getElementById("number-7").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-8
+document.getElementById("callBtn-8").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-8").innerText;
+  const number = document.getElementById("number-8").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
+// card-2
+document.getElementById("callBtn-9").addEventListener("click", function () {
+  let availableCoin = parseInt(document.getElementById("coin-count").innerText);
+  if (availableCoin < 20) {
+    alert("Don't have enough balance to make this call");
+    return;
+  }
+  const newAvailableCoin = availableCoin - 20;
+  document.getElementById("coin-count").innerText = newAvailableCoin;
+  const tag = document.getElementById("tag-9").innerText;
+  const number = document.getElementById("number-9").innerText;
+  alert(`Calling ${tag} ${number}`);
+});
